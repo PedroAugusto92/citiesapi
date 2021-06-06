40548ebfdcc1301e8f8c1ece70d9c5040cee4a53
PedroAugusto92 <pedroaugustob.oliveira@gmail.com>
2021-06-06 17:47:06 -0300

    Projeto criado

---
+++ src/test/java/com/github/pedroaugusto92/citiesapi/CitiesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedroaugusto92.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
